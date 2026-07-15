0a3bf098e761a336c373936e7b4ce6e3a40cbd38
Ninik14 <n_kanashvili@cu.edu.ge>
2026-07-15 23:09:08 +0400

    Improve UI with Bootstrap

--- core/forms.py
+++ core/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Product,UserProfile
+from django.contrib.auth.forms import AuthenticationForm
+
+
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -10,13 +13,52 @@ class RegisterForm(UserCreationForm):
         model = User
         fields = ["username", "email", "password1", "password2"]
     
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
+
+    
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ["title","description","price","category", "image", "status"]
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
+
+        for field in ["category", "status"]:
+            self.fields[field].widget.attrs["class"] = "form-select"
+
+        self.fields["description"].widget.attrs["rows"] = 5
+
+
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ["phone", "bio", "avatar"]
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
+
+        self.fields["bio"].widget.attrs["rows"] = 5
+
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Username" }))
+
+    password = forms.CharField(widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Password",}))
--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
+from .forms import LoginForm
 
 urlpatterns = [
     path("register/", views.register, name="register"),
     path("dashboard/",views.dashboard,name="dashboard"),
-    path("login/", LoginView.as_view(template_name="core/login.html"),name="login"),
+    path("login/", LoginView.as_view(template_name="core/login.html", authentication_form=LoginForm,),name="login"),
     path("logout/", LogoutView.as_view(next_page="login"),name="logout"),
     path("", views.home, name="home"),
     path("catalog/",views.catalog,name="catalog"),
